30f7558180c6f36450982ffea01620067dc50ab1
agundel <git@andreasgundel.de>
2024-03-07 22:38:51 +0100

    refactored wordcount

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
     book_path = "books/Frankenstein.txt"
     text = get_book_text(book_path)
+    count = get_word_count(text)
+    print(f"there are {count} words in Frankenstein")
+
+
+def get_word_count(text):
     words = text.split()
     word_count = 0
     for word in words:
         word_count += 1
-    print(f"there are {word_count} words in Frankenstein")
-#    print(text)
+    return word_count
+
 
 def get_book_text(path):
     with open(path) as f:
