eb73685f5ed06cdda01c0822c159ccbbe559cf6c
agundel <git@andreasgundel.de>
2024-03-07 23:27:55 +0100

    added functionality to sort the lettercount and to report

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 def main():
     book_path = "books/Frankenstein.txt"
     text = get_book_text(book_path)
-#    count = get_word_count(text)
-#    print(f"there are {count} words in Frankenstein")
-    letters = count_letters(text)
-    print(letters)
+    print_report(text, book_path)
 
-def count_letters(text):
+def print_report(text, path):
+    word_count = get_word_count(text)
+    letter_count = sort_dict(get_letter_count(text))
+    
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document\n")
+    for l in letter_count:
+        print(f"The '{l["letter"]}' character was found {l["count"]} times")
+    print("--- End report ---")
+    
+
+
+def sort_dict(dict):
+    sorted_dict = []
+    for d in dict:
+        sorted_dict.append({"letter": d, "count":dict[d]})
+    def sort_on(dict):
+        return dict["count"]
+    sorted_dict.sort(reverse=True, key=sort_on)
+    return sorted_dict
+
+
+
+def get_letter_count(text):
     letter_dict = {}
     lowered_text = text.lower()
     for i in range(len(lowered_text)):
-        if lowered_text[i] not in letter_dict:
+        if lowered_text[i].isalpha() == False:
+            continue
+        elif lowered_text[i] not in letter_dict:
             letter_dict[lowered_text[i]] = 1
         else:
             letter_dict[lowered_text[i]] += 1
     return letter_dict
+
+
 
 def get_word_count(text):
     words = text.split()
