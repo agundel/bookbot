344a8da19e310da006fae22c97cc977421294a8b
agundel <git@andreasgundel.de>
2024-03-07 22:55:08 +0100

    added functionality to count each letter

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 def main():
     book_path = "books/Frankenstein.txt"
     text = get_book_text(book_path)
-    count = get_word_count(text)
-    print(f"there are {count} words in Frankenstein")
+#    count = get_word_count(text)
+#    print(f"there are {count} words in Frankenstein")
+    letters = count_letters(text)
+    print(letters)
 
+def count_letters(text):
+    letter_dict = {}
+    lowered_text = text.lower()
+    for i in range(len(lowered_text)):
+        if lowered_text[i] not in letter_dict:
+            letter_dict[lowered_text[i]] = 1
+        else:
+            letter_dict[lowered_text[i]] += 1
+    return letter_dict
 
 def get_word_count(text):
     words = text.split()
